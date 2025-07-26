80fb643befc640e619d522ef3461f87c0bde660f
Felix Wrba <felix.wrba@web.de>
2025-07-26 11:42:29 +0200

    Added footer, improved design

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
 
   <Header />
 
-  <RouterView />
+  <main class="max-w-4xl m-auto mb-8 h-screen">
+
+    <RouterView />
+
+  </main>
+
+  <Footer />
 
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="bg-slate-100 text-slate-900 p-4">
+    <p>&copy;2025-{{ new Date().getFullYear() }} Felix Wrba. All rights reserved. Contact: <u>fmwrba@gmail.com</u>
+      Github: <a href="http://github.com/FelixWrba" target="_blank" rel="noopener noreferrer"
+        class="underline">@FelixWrba</a></p>
+  </footer>
+</template>
